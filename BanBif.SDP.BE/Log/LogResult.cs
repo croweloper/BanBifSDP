8843f2480c9ef5b37b69c6ffc6f7c747841569f4
Daniel Romero <croweloper@gmail.com>
2022-07-08 20:49:53 -0500

    Update V2

---
+++ BanBif.SDP.BE/Log/LogResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanBif.SDP.BE.Log
+{
+    public class LogResult
+    {
+        public int id { get; set; }
+        public Nullable<int> idregistro { get; set; }
+        public string dni { get; set; }
+        public Nullable<int> paso { get; set; }
+        public string detalle { get; set; }
+        public Nullable<System.DateTime> fecha { get; set; }
+    }
+}
---
+++ BanBif.SDP.DA/Log_AD.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BanBif.SDP.BE.Log;
+using System.Threading.Tasks;
+
+namespace BanBif.SDP.DA
+{
+    public class Log_AD
+    {
+        public LogResponse RegistrarLog(LogResult request)
+        {
+            using (panelEntities db = new panelEntities())
+            {
+
+                LogResponse result = new LogResponse();
+                var obj = db.SDP_RegistrarLog(request.dni, request.idregistro, request.paso, request.detalle);
+
+                result.Data = request;
+
+                if (obj==1)
+                {
+                    result.Mensaje="Guardado Correcto";
+                    result.Result=true;
+                }
+                else
+                {
+                    result.Mensaje="Guardado Incorrecto";
+                    result.Result=false;
+                }
+
+                return result;
+            }
+
+        }
+    }
+}
